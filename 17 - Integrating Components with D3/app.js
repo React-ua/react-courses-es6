b86cc7efe4097c5bdae07ecf14c42fb5cc984681
jeblister <jeblister@gmail.com>
2015-12-26 12:14:46 +0000

    add a variante using jquery ajax

--- 17 - Integrating Components with D3/app.js
+++ 17 - Integrating Components with D3/app.js
@@ -16,16 +16,32 @@ class APP extends React.Component{
 		});
 	}
 	componentWillMount (){
+	//	Using reqwest
 			reqwest({
 				url: 'http://filltext.com/?rows=5&val={randomNumber}',
-				type: 'jsonp',
+				type: 'jsonp', // JSONP (or JSON with Padding) to overcome the cross-domain restrictions imposed by browsers.
 				 crossOrigin: true,
-				success:function(resp){
-					this.setState({data:resp})
-					this.renderChart(this.state.data)
-				}.bind(this)
-			})
-		}
+			success:function(resp){
+				this.setState({data:resp})
+				this.renderChart(this.state.data)
+			}.bind(this)
+		})
+ //Using JQuery
+		// $.ajax({
+		//   type: 'GET',
+		//   url: 'http://filltext.com/?rows=5&val={randomNumber}',
+		// 	dataType:"jsonp",
+		// 	crossDomain: true,
+		//   success: function(resp){
+		// 		this.setState({data:resp})
+		// 	 		this.renderChart(this.state.data)
+		// 	 	}.bind(this),
+		//   error: function() {
+		//     // Here's where you handle an error response.
+		// 		console.log("a error");
+		//   }
+		// });
+}
 	render () {
 		return <div id="chart"></div>
 	}
